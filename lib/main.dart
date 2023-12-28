3cb3600d5ea7c71b646389ec0150beecba315aac
Octane <ashikjoju8055@gmail.com>
2023-12-28 11:06:46 +0530

    added splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -24,64 +24,131 @@ class MyApp extends StatelessWidget {
         Locale('en', 'US'),
       ],
       debugShowCheckedModeBanner: false,
-      theme: const CupertinoThemeData(
-        brightness: Brightness.dark,
-      ),
-      home: CupertinoTabScaffold(
-        tabBar: CupertinoTabBar(
-          items: const [
-            BottomNavigationBarItem(
-              label: 'Lent',
-              icon: Icon(CupertinoIcons.money_dollar),
-            ),
-            BottomNavigationBarItem(
-              label: 'Borrow',
-              icon: Icon(CupertinoIcons.money_dollar_circle),
-            ),
-            BottomNavigationBarItem(
-              label: 'History',
-              icon: Icon(CupertinoIcons.clock),
-            ),
-          ],
-        ),
-        tabBuilder: (BuildContext context, int index) {
-          Widget tabContent;
-          switch (index) {
-            case 0:
-              tabContent = TabOneContent();
-              break;
-            case 1:
-              tabContent = TabTwoContent();
-              break;
-            case 2:
-              tabContent = TabThreeContent();
-              break;
-            default:
-              tabContent = Container();
-          }
+      home: SplashScreen(), // Use SplashScreen as the initial screen
+    );
+  }
+}
 
-          return CupertinoTabView(
-            builder: (BuildContext context) => CupertinoPageScaffold(
-              navigationBar: CupertinoNavigationBar(
-                middle: Text('Debt Manager'),
-                leading: CupertinoButton(
-                  onPressed: () {
-                    // Add your logic for the user icon here
-                  },
-                  child: Icon(CupertinoIcons.person),
-                ),
-                trailing: CupertinoButton(
-                  onPressed: () {
-                    showSettingsAlert(context);
-                  },
-                  child: Icon(CupertinoIcons.settings),
-                ),
-              ),
-              child: tabContent,
-            ),
-          );
-        },
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+class _SplashScreenState extends State<SplashScreen> {
+  double opacityLevel = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+    // Start the fade-in animation
+    Timer(
+      Duration(milliseconds: 500),
+          () {
+        setState(() {
+          opacityLevel = 1.0;
+        });
+      },
+    );
+
+    // Simulate a loading process before navigating to the main screen
+    Timer(
+      Duration(seconds: 3), // Adjust the duration as needed
+          () {
+        // Start the fade-out animation before navigating
+        setState(() {
+          opacityLevel = 0.0;
+        });
+
+        // Navigate to the main screen after the fade-out animation
+        Timer(
+          Duration(milliseconds: 500),
+              () {
+            Navigator.pushReplacement(
+              context,
+              CupertinoPageRoute(builder: (context) => AppTabs()),
+            );
+          },
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      backgroundColor: CupertinoColors.systemGrey5, // Set to gray 800
+      child: Center(
+        child: AnimatedOpacity(
+          opacity: opacityLevel,
+          duration: Duration(milliseconds: 500),
+          child: Image.asset(
+            "../assets/images/logo.png", // Replace with the correct path
+            width: 200, // Adjust the width as needed
+            height: 200, // Adjust the height as needed
+          ),
+        ),
       ),
+    );
+  }
+}
+
+
+class AppTabs extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoTabScaffold(
+      tabBar: CupertinoTabBar(
+        items: const [
+          BottomNavigationBarItem(
+            label: 'Lent',
+            icon: Icon(CupertinoIcons.money_dollar),
+          ),
+          BottomNavigationBarItem(
+            label: 'Borrow',
+            icon: Icon(CupertinoIcons.money_dollar_circle),
+          ),
+          BottomNavigationBarItem(
+            label: 'History',
+            icon: Icon(CupertinoIcons.clock),
+          ),
+        ],
+      ),
+      tabBuilder: (BuildContext context, int index) {
+        Widget tabContent;
+        switch (index) {
+          case 0:
+            tabContent = TabOneContent();
+            break;
+          case 1:
+            tabContent = TabTwoContent();
+            break;
+          case 2:
+            tabContent = TabThreeContent();
+            break;
+          default:
+            tabContent = Container();
+        }
+
+        return CupertinoTabView(
+          builder: (BuildContext context) => CupertinoPageScaffold(
+            navigationBar: CupertinoNavigationBar(
+              middle: Text('Debt Manager'),
+              leading: CupertinoButton(
+                onPressed: () {
+                  // Add your logic for the user icon here
+                },
+                child: Icon(CupertinoIcons.person),
+              ),
+              trailing: CupertinoButton(
+                onPressed: () {
+                  showSettingsAlert(context);
+                },
+                child: Icon(CupertinoIcons.settings),
+              ),
+            ),
+            child: tabContent,
+          ),
+        );
+      },
     );
   }
 }
